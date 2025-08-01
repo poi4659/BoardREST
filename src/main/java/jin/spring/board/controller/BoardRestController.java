583b0f735379e47b2fc47f724a83c537b60e48a5
poi4659 <poi140208@gmail.com>
2025-08-01 21:34:32 +0900

    불필요한 import문 삭제

--- src/main/java/jin/spring/board/controller/BoardRestController.java
+++ src/main/java/jin/spring/board/controller/BoardRestController.java
@@ -4,11 +4,8 @@ package jin.spring.board.controller;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.catalina.loader.ResourceEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jin.spring.board.dto.BoardDTO;
